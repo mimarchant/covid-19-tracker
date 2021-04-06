15e61e75dcb235c8a127b6af86cf30e10caf4f3a
mimarchant <mimarchtt@gmail.com>
2021-04-06 17:48:13 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import Card from "./components/Card";
+
 
 function App() {
+  const [country, setCountry] = useState("");
+  const [data, setData] = useState({});
+  let inputRef = useRef(null);
+  let url = `https://corona.lmao.ninja/v2/countries/${country}?yesterday&strict&query%20`;
+
+  const handleChange = (e) => {
+    setCountry(
+      inputRef.value
+        .trim()
+        .toLowerCase()
+        .replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()))
+    );
+  };
+
+  const getData = (url) => {
+    fetch(url)
+    .then((resp)=>resp.json())
+    .then((data)=>{
+      console.log(data)
+      setData(data)
+    })
+    .catch((err)=>console.log(err))
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getData(url);
+
+    inputRef.value = "";
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 className="text-danger text-center">hola</h1>
+        <div className="text-center">
+          <form onSubmit={handleSubmit}>
+            <input
+              ref={(r) => (inputRef = r)}
+              onChange={handleChange}
+              name="country"
+              type="text"
+            />
+            <label htmlFor="country">Selecciona un pais</label>
+            <button type="submit">submit</button>
+          </form>
+        </div>
+        <Card name={data.country} flag={data.countryInfo.flag} abr={data.countryInfo.iso2} population={data.population}/>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Card = (props) => {
+  return (
+    <div class="card" style={{ width: "18rem" }}>
+      <img class="card-img-top" src={props.flag} alt="Card image cap" />
+      <div class="card-body">
+        <h5 class="card-title">{props.name} ({props.abr})</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Population: {props.population} people</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div class="card-body">
+        <a href="#" class="card-link">
+          Card link
+        </a>
+        <a href="#" class="card-link">
+          Another link
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
